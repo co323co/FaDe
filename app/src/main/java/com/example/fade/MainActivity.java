559363211deef50e8037ae4cfb61fe978a56fc27
co323co <co323co@naver.com>
2020-08-02 03:45:25 +0900

    메뉴바까지 구현 TODO::메뉴에 들어갈 리스트뷰

--- app/src/main/java/com/example/fade/MainActivity.java
+++ app/src/main/java/com/example/fade/MainActivity.java
@@ -1,16 +1,59 @@
 package com.example.fade;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
+    DrawerLayout drawerLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
+        //서랍에 연결해주면, 원래 배경은 어차피 서랍에서 백(activity_main.xml)으로 인클루드 해줌
+        setContentView(R.layout.drawer_main);
 
+        //툴바 설정
+        Toolbar tb = (Toolbar) findViewById(R.id.main_toolbar) ;
+        setSupportActionBar(tb);
+        ActionBar ab = getSupportActionBar() ;
+        ab.setDisplayShowTitleEnabled(false);
+        //이걸 해줘야 사용자가 버튼을 누를 수 있음
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        //메뉴 버튼 설정
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_menu);
+
+        drawerLayout = findViewById(R.id.drawerLayout_main);
+    }
+
+    //메뉴 버튼을 눌렀을 때 드로우가 열리도록 해줌
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int item_id = item.getItemId();
+
+        if(item_id==android.R.id.home) {
+            if(drawerLayout.isDrawerOpen(GravityCompat.START)) {
+                drawerLayout.closeDrawer(GravityCompat.START);
+            }
+            else {
+                drawerLayout.openDrawer(GravityCompat.START);
+            }
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        //다른 아이템이 클릭되었을 시 닫아줌
+        drawerLayout.closeDrawer(GravityCompat.START);
     }
 }

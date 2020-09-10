913b6c9bbb477f3d5e3d1fb13145b043254dabda
co323co <56910798+co323co@users.noreply.github.com>
2020-09-11 05:48:36 +0900

    retrunData수정

--- app/src/main/java/com/example/fade/Server/ReturnData.java
+++ app/src/main/java/com/example/fade/Server/ReturnData.java
@@ -5,26 +5,30 @@ import java.util.ArrayList;
 public class ReturnData {
 
     private String db0, db1, db2;
+    private boolean result;
     private ArrayList<String> gidList;
 
+
     //JSON key 문자열이랑 변수명랑 이름이 같아야함
-    public ReturnData(String  db0, String db1, String db2, ArrayList<String> gidList) {
+    public ReturnData(String  db0, String db1, String db2, boolean result, ArrayList<String> gidList) {
         this.db0=db0;
         this.db1=db1;
         this.db2=db2;
+        this.result=result;
         this.gidList=gidList;
     }
     public ArrayList<String> getDB(){
         ArrayList<String> dbFileList=new ArrayList<>();
         if(db0!=null)
-            dbFileList.add(db0);
+        dbFileList.add(db0);
         dbFileList.add(db1);
         dbFileList.add(db2);
         return dbFileList;
     }
-
-    public  ArrayList<String> getGidList()
-    {
-        return  gidList;
+    public boolean getResult(){
+        return result;
+    }
+    public ArrayList<String> getGidList(){
+        return gidList;
     }
 }

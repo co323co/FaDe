483cbe1e0b87942c0b9a24de3e12344788e5c3ba
hyelmy <herym3567@gmail.com>
2020-10-05 22:29:18 +0900

    테이블 생성 시 연결 x 문제

--- DB/models.py
+++ DB/models.py
@@ -23,8 +23,10 @@ class Group(Base):
     name = Column(Text)
     favorites = Column(Integer, nullable=False)
 
-    uid = Column(Integer, ForeignKey('User.id',ondelete='CASCADE'))
+    uid = Column(Integer, ForeignKey('user.id',ondelete='CASCADE'))
     group_infos = relationship("Group_info")
+
+    users = relationship("User", foreign_keys=[uid])
 
     def __init__(self, uid, name=None, favorites = 0):
         self.uid = uid
@@ -38,7 +40,10 @@ class Person(Base):
     name = Column(Text)
     thumbnail = Column(LONGBLOB)
     
-    uid = Column(Integer, ForeignKey('User.id',ondelete='CASCADE'))
+    uid = Column(Integer, ForeignKey('user.id',ondelete='CASCADE'))
+
+    users = relationship("User", foreign_keys=[uid])
+
 
     def __init__(self, uid, name=None, thumbnail=None):
         self.uid = uid
@@ -47,10 +52,27 @@ class Person(Base):
 
 
 group_person = Table('group_person', Base.metadata,
-    Column('gid', Integer, ForeignKey('Group.id', ondelete='CASCADE')),
-    Column('pid', Integer, ForeignKey('Person.id',ondelete='CASCADE'))
-)
+    Column('gid', Integer, ForeignKey('group.id', ondelete='CASCADE')),
+    Column('pid', Integer, ForeignKey('person.id',ondelete='CASCADE'))
 
+)
+"""
+class group_person(Base):
+    __tablename__ = 'group_person'
+
+
+    gid = Column(Integer, ForeignKey('Group.id',ondelete='CASCADE',primary_key=True))
+    pid = Column(Integer, ForeignKey('Person.id',ondelete='CASCADE',primary_key=True))
+
+    groups = relationship("Group", foreign_keys=[gid])
+    persons = relationship("Person", foreign_keys=[pid])
+
+#mname 모델파일 이름, fnum 모델에 학습된 얼굴 개수
+    def __init__(self, gid, pid):
+        self.gid = gid
+        self.pid = pid
+
+"""
 
 #csv파일 대신하는 테이블
 class Group_info(Base):
@@ -58,7 +80,9 @@ class Group_info(Base):
 
     mname = Column(Text)
     fnum = Column(Integer)
-    gid = Column(Integer, ForeignKey('Group.id',ondelete='CASCADE'),primary_key = True)
+    gid = Column(Integer, ForeignKey('group.id',ondelete='CASCADE'),primary_key = True)
+
+    groups = relationship("Group", foreign_keys=[gid])
 
 #mname 모델파일 이름, fnum 모델에 학습된 얼굴 개수
     def __init__(self, mname, fnum, gid):

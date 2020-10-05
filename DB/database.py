6824e7fa5a337cf29d977689ac2a2a8e47752ed9
hyelmy <herym3567@gmail.com>
2020-10-05 20:55:26 +0900

    db 생성 x 테스트

--- DB/database.py
+++ DB/database.py
@@ -19,7 +19,7 @@ engine = create_engine(BASIC_URL,echo=True, convert_unicode=True)
 print("셀렉트문 전")
 result = engine.execute("SELECT 1 FROM Information_schema.SCHEMATA WHERE SCHEMA_NAME = '%s'"%db['database']).first()
 print("셀렉트문 후")
-if result not None: #윈도우에선 not으로 했지만 리눅스에선 is not None으로 해야 함
+if result is None: #윈도우에선 not으로 했지만 리눅스에선 is not None으로 해야 함
     print("리절트 존재", result[0])
     engine.execute("CREATE DATABASE fade")
 
